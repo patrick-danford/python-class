ede295be1117e23017055a66932097c8a36e6c77
patrick-danford <pdanford@hotmail.com>
2017-03-20 20:39:49 -0700

    3-20

---
+++ book-notes.py
@@ -0,0 +1,75 @@
+#Ch 3.1
+"""
+Any number raised to the 0 power results in 1
+"""
+
+#notes:
+"""product = 1
+for count in xrange(4):
+    product = product * (count + 1)
+    print(product)
+
+product2 = 1
+for count in xrange(1,5):
+    product = product * (count + 2)
+    print product
+
+for blamo in "Hello":
+    print blamo
+
+sum = 0
+for count in xrange(2,20,2):
+    sum = sum + count
+    print sum
+
+for count in xrange(10,0,-1):
+    print count
+
+range(10,1,-1)"""
+
+#exersises
+for count in xrange(5):
+    print count + 1
+
+print "\n"
+
+for count in xrange(1,4):
+    print count
+
+print "\n"
+
+for count in xrange(1, 6, 2):
+    print count + 1
+
+print "\n"
+
+for name in range(100):
+    name = "Patrick" + '\n'
+    print name
+
+for characters in range (1,128):
+    print chr(characters)
+
+testString = "This is a test"
+for convert in testString:
+    print ord(convert)
+
+#ch 3.2
+
+amount = 24.325
+print "Your salary is $%7f" % amount
+
+x = 1
+y = 2
+z = 3
+print "%18d" % x, y, z
+
+salaries = [100, 200, 300, 400]
+
+for numbers in salaries:
+    print "%12.2f" % numbers
+
+#Ch 3.3
+
+
+
