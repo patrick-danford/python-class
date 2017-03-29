4a35c379a50050002a1eeb49d7ddbc7fe3de4a1b
pdanford <pdanford@hotmail.com>
2017-03-29 15:12:49 -0700

    3/29

--- class-notes.py
+++ class-notes.py
@@ -4,14 +4,31 @@ Look into len, enumerate, min, max, tuples, generator, lazy evaluation,
 Play around with debugger
 """
 
-stock_prices_yesterday = [10,20,30,40,30,20,10,20]
-maxvalue = max(stock_prices_yesterday)
-minvalue = min(stock_prices_yesterday)
-print (maxvalue, minvalue)
-print stock_prices_yesterday.index(maxvalue)
+# stock_prices_yesterday = [10,20,30,40,30,20,10,20]
+# maxvalue = max(stock_prices_yesterday)
+# minvalue = min(stock_prices_yesterday)
+# print (maxvalue, minvalue)
+# print stock_prices_yesterday.index(maxvalue)
+#
+#
+#
+# #for i in stock_prices_yesterday:
 
 
+# March 28
 
-#for i in stock_prices_yesterday:
+import argparse
 
+"""
+If main guard = When python interpereter reads a file it executes all of it. the
+if __name__ = __main__ is implemented it will allow you to restict when the
+imported file gets executed
+"""
+"""
+Look at argparse.  What does it actually do?
+Look at raising an exception intentionally
+
+"""
+
+type (2.01)
 
