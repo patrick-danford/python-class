08f75f1820951b69c49a4720581c773a46ba9b9d
pdanford <pdanford@hotmail.com>
2017-04-17 18:05:46 -0700

    4/11

--- class-notes.py
+++ class-notes.py
@@ -50,23 +50,61 @@ Look ar geek stuff, 50 most commonly used commands
 
 """
 
-
-def sum(lower, upper, margin):
-    blanks = " " * margin
-    print blanks, lower, upper
-    if lower > upper:
-        print blanks, 0
-        return 0
-    else:
-        result = lower + sum(lower + 1, upper, margin + 4)
-        print blanks, result
-        return result
-
-sum (1, 4, 0
-     )
+#
+# def sum(lower, upper, margin):
+#     blanks = " " * margin
+#     print blanks, lower, upper
+#     if lower > upper:
+#         print blanks, 0
+#         return 0
+#     else:
+#         result = lower + sum(lower + 1, upper, margin + 4)
+#         print blanks, result
+#         return result
+#
+# sum (1, 4, 0
+#      )
 
 
 #April 13
 """
 Book to read - the pragmatic programmer, Andy Hunt
+
+
+Midterm
+10 problems
+1 - what does the code do.  Will get code, name the function/purpose
+2 - write some test cases for a function.  write a function, write some tests. empty, odd even, 
+    duplicates, negative, etc.  use the assert function
+3 - Find the bugs.   Will get some code, find the bugs. Go over the "what kind of bugs" link posted
+    in discussion.  syntax errors, range errors, could be a logical error (no error repoted in compiler, 
+    but wrong answer given
+4 - Will get a function or two.  We need to refactor - make it more efficient, easier to read, etc. 
+    Refactoring is making the code better, more readble, more efficient etc. 
+5 - Will get snippets of code, will need to say what the output is.  Will be base python, built in functions
+6 - write a function to ...
+7 - write a function to ... 
+8 - Design a problem.  use case, features, psudocode, descriptions.  No code required really.  Write down 
+    requirements and assumptions
+9 - General programming question
+10 - General programming question, like why do we write functions?  or general Qs about software dev, etc
+
+other notes for test:  dont worry about classes.  Ends at higher order functions
+We don't get to use a computer!!   Up to ch 6
+
 """
+
+
+#function that takes a function as input
+
+# def times_2(n):
+#     return n * 2
+#
+# def greater_than_5(n):
+#     if n > 5:
+#         return n
+# print filter(greater_than_5, range(10))
+# print(map(times_2, range(10)))
+
+# lambda - anonymous function
+
